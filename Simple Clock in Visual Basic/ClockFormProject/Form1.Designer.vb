56fae956d49698545996775e71b8799aa03d1df8
Daniel <tolleyd0329@warriormail.rlc.edu>
2015-09-01 17:44:08 -0500

    Simple Desktop Clock that speaks
    Click Time or Date to have it speak.

---
+++ Simple Clock in Visual Basic/ClockFormProject/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblclose = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.BackColor = System.Drawing.Color.Transparent
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.ForeColor = System.Drawing.Color.Lime
+        Me.lblTime.Location = New System.Drawing.Point(32, 24)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(219, 42)
+        Me.lblTime.TabIndex = 1
+        Me.lblTime.Text = "hh:mm:ss tt"
+        Me.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        '
+        'lblclose
+        '
+        Me.lblclose.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblclose.AutoSize = True
+        Me.lblclose.BackColor = System.Drawing.Color.Transparent
+        Me.lblclose.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblclose.ForeColor = System.Drawing.Color.Lime
+        Me.lblclose.Location = New System.Drawing.Point(217, 156)
+        Me.lblclose.Name = "lblclose"
+        Me.lblclose.Size = New System.Drawing.Size(48, 16)
+        Me.lblclose.TabIndex = 3
+        Me.lblclose.Text = "Close"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(307, 22)
+        Me.Panel1.TabIndex = 4
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.BackColor = System.Drawing.Color.Transparent
+        Me.lblDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.ForeColor = System.Drawing.Color.Lime
+        Me.lblDate.Location = New System.Drawing.Point(32, 104)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(209, 25)
+        Me.lblDate.TabIndex = 5
+        Me.lblDate.Text = "ddd MMMM d yyyy"
+        Me.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(22, 153)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Time"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(116, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Date"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(307, 185)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblclose)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lblclose As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblDate As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Simple Clock in Visual Basic/ClockFormProject/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+
+    Dim NewPoint As New System.Drawing.Point
+    Dim X, Y As Integer
+    
+    Private Sub lblclose_Click(sender As System.Object, e As System.EventArgs) Handles lblclose.Click
+        '' Closes Program
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        '' Set labels to Current Date and Time
+        lblTime.Text = Format(Now, "hh:mm:ss tt")
+        lblDate.Text = Date.Now.ToString("ddd MMMM d yyyy ")
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ''Starts time on load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseDown
+        ''For Form Panel Movement
+        X = Control.MousePosition.X - Me.Location.X
+        Y = Control.MousePosition.Y - Me.Location.Y
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseMove
+        ''For Form Panel Movement
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            NewPoint = Control.MousePosition
+            NewPoint.X -= (X)
+            NewPoint.Y -= (Y)
+            Me.Location = NewPoint
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As System.Object, e As System.EventArgs) Handles Label1.Click
+        '' Time Label text to speech
+        Dim SAPI As Object
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.speak(lblTime.Text)
+    End Sub
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs) Handles Label2.Click
+        '' Date Label to speech
+        Dim SAPI As Object
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.speak(lblDate.Text)
+    End Sub
+
+    
+End Class
